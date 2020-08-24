c6d4e945c10dd73e607d1a69a60e23eeead94fa3
huangzhongjian <huangzj08@gmail.com>
2020-08-24 10:29:02 +0800

    feat: vuex tabbar

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,10 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    showTabbar: true,
+    activeTabbar: 'home'
+  },
   mutations: {},
   actions: {},
   modules: {}
