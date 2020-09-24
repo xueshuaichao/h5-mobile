2ffc3e8428077d13838bf7a8cb31aa7d6550b472
mengnan1234 <zmn12345>
2020-09-24 16:29:12 +0800

    fix: 答题

---
+++ src/views/exam/answer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="answer-page">
+        <div class="page-top">
+            <img
+                class="top-img"
+                src="@/assets/exam/instructions-top.png"
+            />
+        </div>
+        答题
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang="less">
+.instructions-page {
+    .page-top {
+        background: #FE9F5A;
+    }
+    .top-img {
+        width: 100%;
+    }
+}
+</style>
